bb136888de8894ec54bc8687526d52cbea359cfd
yog-pande <70682331+yog-pande@users.noreply.github.com>
2024-03-31 14:44:01 +0530

    Add project files.

---
+++ Controllers/DemoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TemplateIntegration.Controllers
+{
+    public class DemoController : ApiController
+    {
+        List<string> li = new List<string>() { "Yagnesh", "Ruchi", "Pruthvi" };
+
+        public List<string> GetStringData()
+        {
+            return li;
+        }
+
+        public List<int> GetIntData()
+        {
+            return new List<int>() { 100,200,300,400,500 };
+        }
+
+        [HttpPost]
+        public List<string> PostData(string name)
+        {
+            li.Add(name);
+            return li;
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TemplateIntegration.Global" Language="C#" %>
